376aadf604e3b66a08475e934e679b6d4477e5bb
AlvinOctaviano16 <octaviano1604@gmail.com>
2025-07-08 15:05:43 +0800

    Berhasil menambahkan fungsi add_item() dan delete_teim()

--- dekstop_app/data/data_manager_sqlite.py
+++ dekstop_app/data/data_manager_sqlite.py
@@ -30,9 +30,47 @@ def get_all_item():
             connection.close()
     return []
 
+def add_item(kode:str,nama:str,harga:int,stock:int):
+    """Fungsi menambahkan item baru ke database"""
+    connection=create_Connection();
+    if connection is not None:
+        try:
+            kursor=connection.cursor()
+            query="INSERT INTO item(kode_item,nama_item,harga_item,stock_item) VALUES (?,?,?,?)"
+            kursor.execute(query, (kode,nama,harga,stock))
+            connection.commit()
+
+            print(f"Item {nama} berhasil ditambahkan")
+            return True
+        except sqlite3.Error as e:
+            print(f"Error saat menambahkan item : {e}")
+            return False
+        finally :
+            connection.close()
+    return False
+
+def delete_item(kode:str):
+    """Fungsi untuk menghapus item berdasarkan kode_item"""
+    connection=create_Connection()
+    if connection is not None:
+        try:
+            kursor=connection.cursor()
+            query="DELETE FROM item WHERE kode_item=?"
+            kursor.execute(query,(kode,))
+            connection.commit()
+            print(f"Item dengan kode {kode} berhasil dihapus")
+            return True
+        except sqlite3.Error as e:
+            print(f"Error saat menghapus item : {e}")
+            return False
+        finally:
+            connection.close()
+    return False 
 
 if __name__=="__main__":
-    print("Testing fungsi get_all_item()...")
+    print("Testing fungsi terbaru")
+    add_item("O0003","Obeng A",11000,25)
+    delete_item("O0003")
     products=get_all_item()
 
     if products:
